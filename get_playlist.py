47ec6e1f2497e90fdcdae53b13246f5b36c1e52f
Tom Charlton <tcharlton90@gmail.com>
2019-11-12 16:01:51 +0000

    Update get_playlist.py

--- get_playlist.py
+++ get_playlist.py
@@ -1,6 +1,10 @@
 import requests
 
-req = requests.get('https://www.last.fm/player/station/user/Fuzwold/recommended')
+#url = 'https://www.last.fm/player/station/user/Fuzwold/library'
+url = 'https://www.last.fm/player/station/user/Fuzwold/mix'
+#url = 'https://www.last.fm/player/station/user/Fuzwold/recommended'
+
+req = requests.get(url)
 
 for pl_item in req.json()['playlist']:
   print(f"Artist: {pl_item['artists'][0]['name']}, title: {pl_item['name']}")
