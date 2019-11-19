41a3b48d9e196bf63d147598e5f825efdb3003a7
Tom Charlton <tcharlton90@gmail.com>
2019-11-19 09:30:56 +0000

    Add migrations

--- app/models.py
+++ app/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 # Create your models here.
-Class Song(models.Model):
+class Song(models.Model):
   title = models.CharField(max_length=100)
   artist = models.CharField(max_length=100)
   song_id = models.CharField(max_length=50)
   
-Class Playlist(models.Model):
+class Playlist(models.Model):
   songs = models.ManyToManyField(Song)
