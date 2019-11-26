bb68f6f3024b4c7292ad1c2261df789d63811773
tcharlton91 <fuzwold@gmail.com>
2019-11-26 11:21:16 +0000

    Fix imports

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-import libraries.get_playlist
+from .libraries.get_playlist import get_last_fm_recommendations, lastFMStation
 
 # Create your views here.
 
