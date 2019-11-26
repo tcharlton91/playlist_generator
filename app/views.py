0469289d0d776aaec906dacffe8dcf6d67979ab9
tcharlton91 <fuzwold@gmail.com>
2019-11-26 16:03:33 +0000

    Made table

--- app/views.py
+++ app/views.py
@@ -6,4 +6,24 @@ from .libraries.get_playlist import get_last_fm_recommendations, lastFMStation
 # Create your views here.
 
 def index(request):
-    return HttpResponse('<br >'.join([str(x) for x in get_last_fm_recommendations(lastFMStation.RECOMMENDED).items()]))
+    return HttpResponse('''
+    <head></head>
+    <body>
+    <table>
+    <tr>
+    <td>Library</td>
+    <td>Mix</td>
+    <td>Recommended</td>
+    </tr>
+    <tr>
+    <td>'''
+    '<br >'.join([str(x) for x in get_last_fm_recommendations(lastFMStation.LIBRARY).items()])
+    '</td><td>'
+    '<br >'.join([str(x) for x in get_last_fm_recommendations(lastFMStation.MIX).items()])
+    '</td><td>'
+    '<br >'.join([str(x) for x in get_last_fm_recommendations(lastFMStation.RECOMMENDED).items()])
+    '''</td>
+    </tr>
+    </table>
+    </body>'''
+    )
