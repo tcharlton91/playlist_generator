347fa39acbf55927cebe009e59b7c8b756569f57
tcharlton91 <fuzwold@gmail.com>
2019-11-26 11:19:01 +0000

    Add return of plalist

---
+++ app/libraries/get_playlist.py
@@ -0,0 +1,25 @@
+import requests
+
+from enum import Enum
+
+class lastFMStation(Enum)
+    LIBRARY = 1
+    MIX = 2
+    RECOMMENDED = 3
+
+urls = {
+    lastFMStation.LIBRARY: 'https://www.last.fm/player/station/user/Fuzwold/library',
+    lastFMStation.MIX: 'https://www.last.fm/player/station/user/Fuzwold/mix',
+    lastFMStation.RECOMMENDED: 'https://www.last.fm/player/station/user/Fuzwold/recommended',
+}
+
+def get_last_fm_recommendations(station):
+    req = requests.get(url)
+
+    resp = {pl_item['artists'][0]['name']:lpl_item['name'] for pl_item in req.json()['playlist']}
+    #resp = {}
+    #for pl_item in req.json()['playlist']:
+    #    #print(f"Artist: {pl_item['artists'][0]['name']}, title: {pl_item['name']}")
+    #    resp[pl_item['artists'][0]['name']] = pl_item['name']
+
+    return resp
--- app/views.py
+++ app/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+import libraries.get_playlist
+
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello world")
+    return HttpResponse(get_last_fm_recommendations(lastFMStation.RECOMMENDED))
