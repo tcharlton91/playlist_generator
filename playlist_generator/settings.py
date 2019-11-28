44fe2a41b25bca5e61d50d3cccd9c9cbf04b5f46
tcharlton91 <fuzwold@gmail.com>
2019-11-28 10:48:38 +0000

    fix paths

--- playlist_generator/settings.py
+++ playlist_generator/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app',
+    'playlist_generator.app',
     'rest_framework',
 ]
 
