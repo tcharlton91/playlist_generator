4cbfaba8b9dccdd20efbac17a2d0c37cfb4836ea
tcharlton91 <fuzwold@gmail.com>
2019-11-28 11:25:06 +0000

    Rename model fields

--- playlist_generator/app/models.py
+++ playlist_generator/app/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Song(models.Model):
-  title = models.CharField(max_length=100)
-  artist = models.CharField(max_length=100)
+  song_title = models.CharField(max_length=100)
+  song_artist = models.CharField(max_length=100)
   song_id = models.CharField(max_length=50)
   
 class Playlist(models.Model):
-  songs = models.ManyToManyField(Song)
+  playlist_songs = models.ManyToManyField(Song)
