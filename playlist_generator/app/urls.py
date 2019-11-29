6d76855a21461d876559d182ef30c3d7f9a8397d
tcharlton91 <fuzwold@gmail.com>
2019-11-29 16:12:53 +0000

    Add url for view

--- playlist_generator/app/urls.py
+++ playlist_generator/app/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
         path('basic', views.basicView),
         path('library', views.libraryView),
+        path('libraryTemp', views.libraryTemplateView),
         path('mix', views.mixView),
         path('recommended', views.recommendedView),
         path('', views.basicView),
