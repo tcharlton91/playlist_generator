4f745adc76720a0d252c9672c172f4258b9ee749
tcharlton91 <57965561+tcharlton91@users.noreply.github.com>
2019-12-02 13:32:12 +0000

    Update urls.py

--- playlist_generator/urls.py
+++ playlist_generator/urls.py
@@ -21,6 +21,6 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
-    path('', TemplateView.as_view(template_name='index.html')),
+    path('', TemplateView.as_view(template_name='base.html')),
     path('app/', include('playlist_generator.app.urls'))
 ]
