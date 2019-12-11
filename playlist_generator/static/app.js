ca054a928b60f4a9e244567f050644f741f74120
tcharlton91 <fuzwold@gmail.com>
2019-12-11 12:38:35 +0000

    Added getting GMusic auth code and storing in cookie

--- playlist_generator/static/app.js
+++ playlist_generator/static/app.js
@@ -1,10 +1,18 @@
-//document.getElementById("defaultTab").click();
 
-// const node = document.getElementById("lastFMUsername");
-// node.addEventListener("keyup", function(event) {
-//     if (event.key === "Enter") {
-//         var x = document.getElementById("lastFMUsername").value;
-//         document.getElementById("demo").innerHTML = x
-        
-//     }
-// });
+if ( checkCookie('gmusicAuthCode') ) {
+    $('.GMusicAuth').hide();
+    $('.GMusicRefreshAuth').show();
+}
+else {
+    $('.GMusicAuth').show();
+    $('.GMusicRefreshAuth').hide();
+}
+
+function checkCookie($name)
+{
+    if (typeof $.cookie($name) === 'undefined') {
+        return false;
+    } else {
+        return true;
+    }
+}
