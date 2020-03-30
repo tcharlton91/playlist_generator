e8f076ced908f774b37cd8eb20913d75de678348
tcharlton91 <fuzwold@gmail.com>
2020-03-30 14:35:08 +0000

    Fix comma

--- playlist_generator/app/urls.py
+++ playlist_generator/app/urls.py
@@ -6,6 +6,6 @@ from playlist_generator.app.libraries.submit_gmusic_playlist import perform_oaut
 urlpatterns = [
         path('', views.baseView),
         path('performOauth', perform_oauth_and_get_credentials),
-        path('importPlaylist', import_playlist)
+        path('importPlaylist', import_playlist),
         path('login', login),
         ]
